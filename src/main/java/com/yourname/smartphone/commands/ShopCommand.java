790bdf0a75bc4d20d661bf34116e910c810d0db4
XenzzMC <rifkydanangrizqullah@gmail.com>
2026-04-05 20:06:09 +0700

    Create ShopCommand.java

---
+++ src/main/java/com/yourname/smartphone/commands/ShopCommand.java
@@ -0,0 +1,16 @@
+package com.yourname.smartphone.commands;
+
+import com.yourname.smartphone.gui.ShopMenu;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ShopCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) return true;
+        new ShopMenu().open((Player) sender);
+        return true;
+    }
+}
